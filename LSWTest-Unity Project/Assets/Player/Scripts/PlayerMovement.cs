182250760ef82680f502cf3fbdff6eecb588b09a
Nicolas Ferreiro <41026986+srezu@users.noreply.github.com>
2021-11-23 19:09:25 -0300

    Update settings

--- LSWTest-Unity Project/Assets/Player/Scripts/PlayerMovement.cs
+++ LSWTest-Unity Project/Assets/Player/Scripts/PlayerMovement.cs
@@ -11,14 +11,21 @@ public class PlayerMovement : MonoBehaviour
    
    private Rigidbody2D _rb;
    private PlayerInput _playerInput;
-
+   private Animator _playerAnim;
 
    private void Awake()
    {
        _rb = GetComponent<Rigidbody2D>();
        _playerInput = GetComponent<PlayerInput>();
+       _playerAnim = GetComponent<Animator>();
    }
-    
+
+   private void Update()
+   {
+       _playerAnim.SetFloat("HorizontalX", _playerInput.movement.x);
+       _playerAnim.SetFloat("VerticalY", _playerInput.movement.y);
+   }
+
    private void FixedUpdate()
    {
        _rb.MovePosition(_rb.position + _playerInput.movement * movementSpeed * Time.fixedDeltaTime);
