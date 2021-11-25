584e3d001ae2e942343f30ac9afb8f467f60a015
Nicolas Ferreiro <41026986+srezu@users.noreply.github.com>
2021-11-24 23:23:27 -0300

    Update drag system

---
+++ LSWTest-Unity Project/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragDrop : MonoBehaviour,IPointerDownHandler,IBeginDragHandler,IEndDragHandler,IDragHandler
+{
+
+    public Canvas canvas;
+
+    public RectTransform _rectTransform;
+    public Vector3 _initialRecTransform;
+
+
+  
+    private bool _onDrag;
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        canvas = FindObjectOfType<Canvas>();
+ 
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (_onDrag) return;
+        _initialRecTransform = _rectTransform.position;
+
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");       
+        _onDrag = true;
+        eventData.pointerDrag.GetComponent<Image>().raycastTarget = false;
+
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        _onDrag = false;
+        _rectTransform.position = _initialRecTransform;
+        eventData.pointerDrag.GetComponent<Image>().raycastTarget = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        _rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+   
+}
--- LSWTest-Unity Project/Assets/Scripts/InventorySlot.cs
+++ LSWTest-Unity Project/Assets/Scripts/InventorySlot.cs
@@ -3,24 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using  UnityEngine.EventSystems;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour,IDropHandler
 {
    [Header(("Slot"))]
    public Image iconRenderer;
    public Button slotButton;
+   public ClothType slotType;//for exclusive types of slots
+   public bool exclusiveSlot;
    [Header(("Clothing data"))]
    public ClothingData clothingData;
-
-  
-
+   
    private void Update()
    {
       UpdateInventorySlot();
-
       
-
-      slotButton.interactable = clothingData != null;
    }
 
    public void UpdateInventorySlot()
@@ -35,5 +33,30 @@ public class InventorySlot : MonoBehaviour
 
       iconRenderer.enabled = true;
       iconRenderer.sprite = clothingData.clothingSprite.clothingSpriteFront;
+   }
+   
+   public void OnDrop(PointerEventData eventData)
+   {
+      if (eventData.pointerDrag != null)
+      {
+         Debug.Log("DROP in " +gameObject.name);
+         var oldInventorySlot = eventData.pointerDrag.gameObject.GetComponentInParent<InventorySlot>();
+         
+         if (oldInventorySlot == this) return;
+
+         if (exclusiveSlot && oldInventorySlot.clothingData.clothingType != slotType) return;
+
+         clothingData = oldInventorySlot.clothingData;
+         oldInventorySlot.Clear();
+      }
+      else
+      {
+         Debug.Log("DROP failed! ");
+      }
+   }
+
+   public void Clear()
+   {
+      clothingData = null;
    }
 }
